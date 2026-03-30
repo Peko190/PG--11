77c72a2b125c72ac43339b55f228cf8bf0c156a3
gabriel <gaboenev@gmail.com>
2026-03-30 21:02:26 +0300

    lessons and courses added

---
+++ Domain/Entites/Courses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG_Тема_11.Domain.Entites
+{
+    public class Courses
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Level { get; private set; }
+
+        public Courses(int id, string title, string description, string level)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Level = level;
+        }
+    }
+}
---
+++ Domain/Entites/Lessons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG_Тема_11.Domain.Entites
+{
+    public class Lessons
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public int Order { get; private set; }
+        public int CourseId { get; private set; }
+
+        public Lessons(string title, string content, int order, int courseId)
+        {
+            Title = title;
+            Content = content;
+            Order = order;
+            CourseId = courseId;
+        }
+
+
+    }
+}
